375559037b116e6b9d0b51b4334081991248dd2f
lavos84 <32783492+lavos84@users.noreply.github.com>
2019-03-18 20:21:20 -0700

    Code works and runs constellations

--- NovaBackend.py
+++ NovaBackend.py
@@ -10,97 +10,190 @@ def ParseQuery(results):
     # First determine if we're querying 1 star, many stars, or a constellation.
     if results["HipparcosID"] != None:
         # This means the user has entered a specific HipparcosID.  We'll want to query that specific star
-        query = ("SELECT * FROM Star WHERE HipparcosID = %i; " %results["HipparcosID"])
+        query = ("SELECT * FROM Star WHERE `HipparcosID` = %s; " %results["HipparcosID"])
     # otherwise it's probably a multistar search
     else:
         # in which case, we'll probably want to piece together the query
         query_WHERE = None
         # Starting with Constellation,
         if results["Constellation"] != None:
-            query_WHERE = "WHERE Constellation = '%s' " %results["Constellation"]
+            query_WHERE = "WHERE `ConstellationID` = '%s' " %results["Constellation"]
         if results["Spectral Type"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE SpectralType = '%s' " %results["Spectral Type"]
+                query_WHERE = "WHERE LEFT(`SpectralType`, 1) = '%s' " %results["Spectral Type"]
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND SpectralType = '%s' " %results["Spectral Type"]
+                query_WHERE += " AND LEFT(`SpectralType`, 1) = '%s' " %results["Spectral Type"]
         # Remember, we need ALL 3 fields or the decimal RA to be present here to work
         if results["RA Hours"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE RA = '%s' " % str(results["RA Hours"])
+                query_WHERE = "WHERE `RA` = %s " % str(results["RA Hours"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND RA = '%s' " % str(results["RA Hours"])
+                query_WHERE += " AND `RA` = %s " % str(results["RA Hours"])
         if results["Dec"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE Dec = '%s' " % str(results["Dec"])
+                query_WHERE = "WHERE `Dec` = %s " % str(results["Dec"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND Dec = '%s' " % str(results["Dec"])
+                query_WHERE += " AND `Dec` = %s " % str(results["Dec"])
         # When checking distance, we'll need to adjust this to accept a range of distances.  That is,
         # from distance1 to distance2.
         if results["Distance"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE Distance = '%s' " % str(results["Distance"])
+                query_WHERE = "WHERE `Distance` = %s " % str(results["Distance"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND Distance = '%s' " % str(results["Distance"])
+                query_WHERE += " AND `Distance` = %s " % str(results["Distance"])
         # Likewise with Magnitude, Absolute Magnitude, and Luminosity.  Ranges are approprite
         if results["Magnitude"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE Magnitude = '%s' " % str(results["Magnitude"])
+                query_WHERE = "WHERE `Magnitude` = %s " % str(results["Magnitude"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND Magnitude = '%s' " % str(results["Magnitude"])
+                query_WHERE += " AND `Magnitude` = %s " % str(results["Magnitude"])
         if results["Absolute Magnitude"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE AbsoluteMagnitude = '%s' " % str(results["Absolute Magnitude"])
+                query_WHERE = "WHERE `AbsoluteMagnitude` = %s " % str(results["Absolute Magnitude"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND AbsoluteMagnitude = '%s' " % str(results["Absolute Magnitude"])
+                query_WHERE += " AND `AbsoluteMagnitude` = %s " % str(results["Absolute Magnitude"])
         if results["Luminosity"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE Luminosity = '%s' " % str(results["Luminosity"])
+                query_WHERE = "WHERE `Luminosity` = %s " % str(results["Luminosity"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND Luminosity = '%s' " % str(results["Luminosity"])
+                query_WHERE += " AND `Luminosity` = %s " % str(results["Luminosity"])
         if results["Min Magnitude"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE VariableMinMagnitude = '%s' " % str(results["Min Magnitude"])
+                query_WHERE = "WHERE `VariableMinMagnitude` = %s " % str(results["Min Magnitude"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND VariableMinMagnitude = '%s' " % str(results["Min Magnitude"])
+                query_WHERE += " AND `VariableMinMagnitude` = %s " % str(results["Min Magnitude"])
         if results["Max Magnitude"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE VariableMaxMagnitude = '%s' " % str(results["Max Magnitude"])
+                query_WHERE = "WHERE `VariableMaxMagnitude` = %s " % str(results["Max Magnitude"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND VariableMaxMagnitude = '%s' " % str(results["Max Magnitude"])
+                query_WHERE += " AND `VariableMaxMagnitude` = %s " % str(results["Max Magnitude"])
         # Star System Name will probably require some prior knowledge since these are not intuitive.
         # Maybe we could somehow incororate a mechanism that checks if the spelling is 'close enough'
         if results["Star System Name"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE BaseName = '%s' " % str(results["Star System Name"])
+                query_WHERE = "WHERE `BaseName` = '%s' " % str(results["Star System Name"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND BaseName = '%s' " % str(results["Star System Name"])
+                query_WHERE += " AND `BaseName` = '%s' " % str(results["Star System Name"])
         # Last one is either 1, 2, or 3 if not null.
         if results["Companions"] != None:
             # Check if there's an existing WHERE clause
             if query_WHERE == None:
-                query_WHERE = "WHERE CompanionID = '%s' " % str(results["Companions"])
+                query_WHERE = "WHERE `CompanionID` = %s " % str(results["Companions"])
             else:
                 # If so, append to it with the AND statement
-                query_WHERE += " AND CompanionID = '%s' " % str(results["Companions"])
-        query = ("SELECT * FROM Star " + query_WHERE + "; ")
+                query_WHERE += " AND `CompanionID` = %s " % str(results["Companions"])
+        query = ("SELECT * FROM Star " + query_WHERE)
     return(query)
+
+def StarType(spectraltype):
+    # Define a few empty lists (or arrays)
+    startype = list(spectraltype)   # This holds the letter of the spectral type: O B A F G K M C or U for Unknown
+    starsize = np.zeros(len(spectraltype))  # This will determine the size of the star on the plot
+    starcolor = ["" for star in range(0, len(spectraltype))]  # Determines color on the plot
+    # Go through every search result and assign the relevant attributes
+    for i in range(0, len(spectraltype)):
+        # Try to grab the first letter of spectral type
+        try:
+            startype[i] = spectraltype[i][0]
+        except:
+        # If it didn't work, it's because the value was null, so we assign it "U" for "Unknown"
+            startype[i] = "U"
+        # Then, depending on the spectral type, assign some meaningful sizes and colors
+        if startype[i] == "O":
+            starsize[i] = 4
+            starcolor[i] = "blue"
+        elif startype[i] == "B":
+            starsize[i] = 3.5
+            starcolor[i] = "darkturquoise"
+        elif startype[i] == "A":
+            starsize[i] = 3
+            starcolor[i] = "paleturquoise"
+        elif startype[i] == "F":
+            starsize[i] = 2.5
+            starcolor[i] = "white"
+        elif startype[i] == "G":
+            starsize[i] = 2
+            starcolor[i] = "yellow"
+        elif startype[i] == "K":
+            starsize[i] = 1.5
+            starcolor[i] = "orange"
+        elif startype[i] == "M":
+            starsize[i] = 1
+            starcolor[i] = "red"
+        elif startype[i] == "C":
+            starsize[i] = 3
+            starcolor[i] = "maroon"
+        else:
+            starsize[i] = 2
+            starcolor[i] = "gold"
+    return(startype, starsize, starcolor)
+
+def MultiStarPlot(query):
+    conn = mysql.connector.connect(user='root', password='Drew$kiWi1kins09!', host='127.0.0.1', database='NovaDB')
+    cursor = conn.cursor()
+
+    # It's important to limit the distance because when distance = 100000 it throws the entire scale off
+    query += (" AND Distance <= 50000; ")
+    cursor.execute(query)
+    result = pd.DataFrame(cursor.fetchall(), columns= ["HipparcosID", "HenryDraperID", "HarvardRevisedID", "GlieseID",
+        "BayerFlamsteed", "ProperName", "RA", "Dec", "Distance", "Magnitude", "AbsoluteMagnitude", "SpectralType",
+        "ColorIndex", "X", "Y", "Z", "RA(radians)", "Dec(radians)", "ConstellationID", "Luminosity", "CompanionID",
+        "PrimaryCompanionID", "BaseName", "VariableStarID", "VariableMinMagnitude", "VariableMaxMagnitude"])
+    cursor.close()
+    conn.close()
+    # Get the attributes for each star on the 3D plot
+    startype, starsize, starcolor = StarType(result["SpectralType"])
+
+    # Code up the plot
+    from mpl_toolkits.mplot3d import Axes3D
+    # Define the 3D figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # Read the data from the query as a list
+    xs = list(pd.to_numeric(result["X"]))
+    ys = list(pd.to_numeric(result["Y"]))
+    zs = list(pd.to_numeric(result["Z"]))
+    # Draw the scatterplot using the colors and sizes we determined above
+    ax.scatter(xs, ys, zs, c= starcolor, s= starsize, depthshade= False)
+    # The axis wont be centered initially, so let's fix that.  Start by getting the max value on each dimension
+    xlim = max(np.abs(ax.get_xlim()))
+    ylim = max(np.abs(ax.get_ylim()))
+    zlim = max(np.abs(ax.get_zlim()))
+    # Then set the axis on each dimension such that 0 is in the middle of the max value
+    ax.set_xlim(-xlim, xlim)
+    ax.set_ylim(-ylim, ylim)
+    ax.set_zlim(-zlim, zlim)
+    # Draw a plane at z= 0.  This is purely for asthetics so you can get a sense of each star's location
+    xx, yy = np.meshgrid(np.arange(-xlim, xlim, 5), np.arange(-ylim, ylim, 5))
+    z = xx * 0
+    ax.plot_wireframe(xx, yy, z, alpha=0.5, rcount= 5, ccount= 5, colors= "w")
+    # Draw lines from the z=0 plane to the star if there's only a few stars.  Otherwise this takes WAY too long.
+    if len(xs) < 1000:
+        for i,j,k in zip(xs, ys, zs):
+            if (i != 0) and (j != 0) and (k != 0):
+                if k >= 0: ax.plot([i,i],[j,j],[0,k],color = 'b', alpha= .3)
+                else: ax.plot([i,i],[j,j],[0,k], color = 'r', alpha= .3)
+    ax.set_facecolor('black')
+    ax.grid(False)
+    ax.set_axis_off()
+    plt.show()
--- NovaFrontend.py
+++ NovaFrontend.py
@@ -35,7 +35,98 @@ def User_Query():
         HipparcosID = None  # it was blank to begin with
     # Check if Constellation is null
     Constellation = request.form['Constellation']
-    if Constellation == "": Constellation = None    # it was blank to begin with
+    if Constellation == "":
+        Constellation = None    # it was blank to begin with
+    else:
+        if Constellation == "Andromeda": Constellation = "And"
+        elif Constellation == "Antlia": Constellation = "Ant"
+        elif Constellation == "Apus": Constellation = "Aps"
+        elif Constellation == "Aquila": Constellation = "Aql"
+        elif Constellation == "Aquarius": Constellation = "Aqr"
+        elif Constellation == "Ara": Constellation = "Ara"
+        elif Constellation == "Aries": Constellation = "Ari"
+        elif Constellation == "Auriga": Constellation = "Aur"
+        elif Constellation == "Bootes": Constellation = "Boo"
+        elif Constellation == "Caelum": Constellation = "Cae"
+        elif Constellation == "Camelopardalis": Constellation = "Cam"
+        elif Constellation == "Capricornus": Constellation = "Cap"
+        elif Constellation == "Carina": Constellation = "Car"
+        elif Constellation == "Cassiopeia": Constellation = "Cas"
+        elif Constellation == "Centaurus": Constellation = "Cen"
+        elif Constellation == "Cepheus": Constellation = "Cep"
+        elif Constellation == "Cetus": Constellation = "Cet"
+        elif Constellation == "Chamaeleon": Constellation = "Cha"
+        elif Constellation == "Circinus": Constellation = "Cir"
+        elif Constellation == "Canis Major": Constellation = "Cma"
+        elif Constellation == "Canis Minor": Constellation = "Cmi"
+        elif Constellation == "Cancer": Constellation = "Cnc"
+        elif Constellation == "Columba": Constellation = "Col"
+        elif Constellation == "Coma Berenicies": Constellation = "Com"
+        elif Constellation == "Corona Australis": Constellation = "Cra"
+        elif Constellation == "Corona Borealis": Constellation = "Crb"
+        elif Constellation == "Crater": Constellation = "Crt"
+        elif Constellation == "Crux": Constellation = "Cru"
+        elif Constellation == "Corvus": Constellation = "Crv"
+        elif Constellation == "Canes Vanatici": Constellation = "Cvn"
+        elif Constellation == "Cygnus": Constellation = "Cyg"
+        elif Constellation == "Delphinus": Constellation = "Del"
+        elif Constellation == "Dorado": Constellation = "Dor"
+        elif Constellation == "Draco": Constellation = "Dra"
+        elif Constellation == "Equuleus": Constellation = "Equ"
+        elif Constellation == "Eridanus": Constellation = "Eri"
+        elif Constellation == "Fornax": Constellation = "For"
+        elif Constellation == "Gemini": Constellation = "Gem"
+        elif Constellation == "Grus": Constellation = "Gru"
+        elif Constellation == "Hercules": Constellation = "Her"
+        elif Constellation == "Horologium": Constellation = "Hor"
+        elif Constellation == "Hydra": Constellation = "Hya"
+        elif Constellation == "Hydrus": Constellation = "Hyi"
+        elif Constellation == "Indus": Constellation = "Ind"
+        elif Constellation == "Lacerta": Constellation = "Lac"
+        elif Constellation == "Leo": Constellation = "Leo"
+        elif Constellation == "Lepus": Constellation = "Lep"
+        elif Constellation == "Libra": Constellation = "Lib"
+        elif Constellation == "Leo Minor": Constellation = "Lmi"
+        elif Constellation == "Lupus": Constellation = "Lup"
+        elif Constellation == "Lynx": Constellation = "Lyn"
+        elif Constellation == "Lyra": Constellation = "Lyr"
+        elif Constellation == "Mensa": Constellation = "Men"
+        elif Constellation == "Microscopium": Constellation = "Mic"
+        elif Constellation == "Monoceros": Constellation = "Mon"
+        elif Constellation == "Musca": Constellation = "Mus"
+        elif Constellation == "Norma": Constellation = "Nor"
+        elif Constellation == "Octans": Constellation = "Oct"
+        elif Constellation == "Ophiuchus": Constellation = "Oph"
+        elif Constellation == "Orion": Constellation = "Ori"
+        elif Constellation == "Pavo": Constellation = "Pav"
+        elif Constellation == "Pegasus": Constellation = "Peg"
+        elif Constellation == "Perseus": Constellation = "Per"
+        elif Constellation == "Phoenix": Constellation = "Phe"
+        elif Constellation == "Pictor": Constellation = "Pic"
+        elif Constellation == "Piscis Austrinus": Constellation = "Psa"
+        elif Constellation == "Pisces": Constellation = "Psc"
+        elif Constellation == "Puppis": Constellation = "Pup"
+        elif Constellation == "Pyxis": Constellation = "Pyx"
+        elif Constellation == "Reticulum": Constellation = "Ret"
+        elif Constellation == "Sculptor": Constellation = "Scl"
+        elif Constellation == "Scropius": Constellation = "Sco"
+        elif Constellation == "Scutum": Constellation = "Sct"
+        elif Constellation == "Serpens Caput": Constellation = "Ser1"
+        elif Constellation == "Serpens Cauda": Constellation = "Ser2"
+        elif Constellation == "Sextans": Constellation = "Sex"
+        elif Constellation == "Sagitta": Constellation = "Sge"
+        elif Constellation == "Sagittarius": Constellation = "Sgr"
+        elif Constellation == "Taurus": Constellation = "Tau"
+        elif Constellation == "Telescopium": Constellation = "Tel"
+        elif Constellation == "Triangulum Australe": Constellation = "Tra"
+        elif Constellation == "Triangulum": Constellation = "Tri"
+        elif Constellation == "Tucana": Constellation = "Tuc"
+        elif Constellation == "Ursa Major": Constellation = "Uma"
+        elif Constellation == "Ursa Minor": Constellation = "Umi"
+        elif Constellation == "Vela": Constellation = "Vel"
+        elif Constellation == "Virgo": Constellation = "Vir"
+        elif Constellation == "Volans": Constellation = "Vol"
+        else: Constellation = "Vul"
     # Check if SpectralType is null
     SpectralType = request.form['SpectralType']
     if SpectralType == "": SpectralType = None      # it was blank to begin with
@@ -43,40 +134,40 @@ def User_Query():
     try:
         RA_Hours = int(request.form['RA_Hours_Minutes_Seconds'])
         # Make sure RA_Hours is between 0-24
-        if (RA_Hours < 0) or (RA_Hours > 24): return redirct("localhost:5000/index.html")
+        if (RA_Hours < 0) or (RA_Hours > 24): return redirect("localhost:5000/index.html")
     except:
         RA_Hours = None     # it was blank to begin with
     # Check if Declination is null
     try:
         Dec = float(request.form['Dec'])
         # Then check if declination is between -90 and 90
-        if (Dec > 90) or (Dec < -90): return redirct("localhost:5000/index.html")
+        if (Dec > 90) or (Dec < -90): return redirect("localhost:5000/index.html")
     except:
         Dec = None          # it was blank to begin with
     # Check if Distance is null
     try:
         Distance = float(request.form['Distance'])
         # Make sure it's a positive distance
-        if Distance < 0: return redirct("localhost:5000/index.html")
+        if Distance < 0: return redirect("localhost:5000/index.html")
     except:
         Distance = None     # it was blank to begin with
     # Check if Magnitude is null
     Magnitude = request.form['Magnitude']
     if Magnitude == "":     # it was blank to begin with
-        return redirct("localhost:5000/index.html")
+        Magnitude = None
     else:
         Magnitude = float(Magnitude)
     # Check if Absolute_Magnitude is null
     Absolute_Magnitude = request.form['Absolute_Magnitude']
     if Absolute_Magnitude == "":    # it was blank to begin with
-        return redirct("localhost:5000/index.html")
+        Absolute_Magnitude = None
     else:
         # Turn the string into a float
         Absolute_Magnitude = float(Absolute_Magnitude)
     # Check if Luminosity is null
     Luminosity = request.form['Luminosity']
     if Luminosity == "":    # it was blank to begin with
-        return redirct("localhost:5000/index.html")
+        Luminosity = None
     else:
         # Turn the string into a float and make sure it's positive
         Luminosity = float(Luminosity)
@@ -84,14 +175,14 @@ def User_Query():
     # Check if Min_Magnitude is null
     Min_Magnitude = request.form['Min_Magnitude']
     if Min_Magnitude == "":    # it was blank to begin with
-        return redirct("localhost:5000/index.html")
+        Min_Magnitude = None
     else:
         # Turn the string into a float
         Min_Magnitude = float(Min_Magnitude)
     # Check if Max_Magnitude is null
     Max_Magnitude = request.form['Max_Magnitude']
     if Max_Magnitude == "":
-        return redirct("localhost:5000/index.html")
+        Max_Magnitude = None
     else:
         # Turn the string into a float
         Max_Magnitude = float(Max_Magnitude)
@@ -109,6 +200,7 @@ def User_Query():
                "Max Magnitude": Max_Magnitude, "Star System Name": Star_System_Name, "Companions": CompanionID}
     query= NovaBackend.ParseQuery(results)
     print(query)
+    NovaBackend.MultiStarPlot(query)
     return(jsonify(results))
 
 ## Starts the server for serving Rest Services
